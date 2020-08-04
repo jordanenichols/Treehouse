3e034c976d609d7d4b48ffaf5e7a161236f17f33
jordanenichols <jordan.jbns@gmail.com>
2020-08-04 08:32:35 -0500

    first commit

---
+++ sdltest/src/GameObject.cpp
@@ -0,0 +1,27 @@
+#include "GameObject.h"
+#include "TextureManager.h"
+GameObject::GameObject(const char* textureSheet, int x, int y) {
+	objTexture = TextureManager::LoadTexture(textureSheet);
+	xpos = x;
+	ypos = y;
+}
+
+void GameObject::update() {
+
+	xpos++;
+	ypos++;
+
+	srcRect.h = 64;
+	srcRect.w = 64;
+	srcRect.x = 0;
+	srcRect.y = 0;
+
+	destRect.x = xpos;
+	destRect.y = ypos;
+	destRect.w = srcRect.w * 2;
+	destRect.h = srcRect.h * 2;
+}
+
+void GameObject::render() {
+	SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
+}
---
+++ sdltest/src/GameObject.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Game.hpp"
+
+class GameObject {
+public:
+	GameObject(const char* textureSheet, int x, int y);
+	~GameObject();
+
+	void update();
+	void render();
+
+private:
+	int xpos;
+	int ypos;
+	SDL_Texture* objTexture;
+	SDL_Rect srcRect, destRect;
+};
